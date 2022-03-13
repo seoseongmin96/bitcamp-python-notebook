c29245d4e48e88d68c9b013e9e6bdf73f5f011e1
seoseongmin96 <justin090452@gmail.com>
2022-03-13 20:25:34 +0900

    quiz 30 complete

--- hello/quiz30.py
+++ hello/quiz30.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from icecream import  ic
+from icecream import ic
 
 class Quiz30:
     '''
@@ -11,12 +11,31 @@ class Quiz30:
             4  10  11  12
     '''
     def quiz30_df_4_by_3(self) -> str:
+        '''
         df = pd.DataFrame([[1,2,3],
                           [4,5,6],
                           [7,8,9],
                           [10,11,12]], index=range(1,5), columns=['A','B','C'])
         # 위 식을 리스트결합 형태로 분해해서 조립하시오
-        ic(df)
+
+
+        ic(df)'''
+
+        l1 = []
+        l2 = []
+        l3 = []
+        l4 = []
+        [l1.append(i) for i in range(1,4)]
+        [l2.append(i) for i in range(4,7)]
+        [l3.append(i) for i in range(7,10)]
+        [l4.append(i) for i in range(10,13)]
+
+        e = {'1' : l1, '2' : l2, '3' : l3, '4' : l4}
+        df = pd.DataFrame.from_dict(e, orient='index', columns=['A', 'B', 'C'])
+        print(df)
+
+
+
         return None
 
     '''
