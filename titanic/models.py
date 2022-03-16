6c0f91a558a1c6406fcf2e4048b472c9723153ee
seoseongmin96 <justin090452@gmail.com>
2022-03-16 15:00:26 +0900

    template method pattern

--- titanic/models.py
+++ titanic/models.py
@@ -1,5 +1,6 @@
 from icecream import ic
 
+import titanic
 from context.domains import Dataset
 from context.models import Model
 
@@ -10,6 +11,7 @@ class TitanicModel (object):
         self.dataset = Dataset()
         self.train = self.model.new_model(train_fname)
         self.test = self.model.new_model(test_fname)
+
         # id 추출
 
         ic(f'트레인 컬럼 {self.train.columns}')
@@ -18,64 +20,75 @@ class TitanicModel (object):
 
 
     def preprocess(self):
-        self.sib_sp_garbage()
-        self.parch_garbage()
-        self.ticket_garbage()
-        self.cabin_garbage()
-        self.create_label()
-        self.create_label()
-        self.pclass_ordinal()
-        self.name_nominal()
-        self.sex_nominal()
-        self.age_ratio()
-        self.fare_ratio()
-        self.embarked_nominal()
+        df = self.train
+        df = self.drop_feature(df)
+        df = self.create_label(df)
+        df = self.create_train(df)
+        df = self.pclass_ordinal(df)
+        df = self.name_nominal(df)
+        df = self.sex_nominal(df)
+        df = self.age_ratio(df)
+        df = self.fare_ratio(df)
+        df = self.embarked_nominal(df)
+        return df
 
 
+    @staticmethod
+    def create_label(df) -> object:
+        return df
 
-    def create_label(self):
-        pass
+    @staticmethod
+    def create_train(df) -> object:
+        return df
 
-    def create_train(self) -> object:
-        pass
+    def drop_feature(self, df) -> object:
+        a = [ i for i in []]
 
-    def drop_feature(self) -> object:
-        pass
+
+        self.sibsp_garbage(df)
+        self.parch_garbage(df)
+        self.ticket_garbage(df)
+        self.cabin_garbage(df)
+        return df
 
     '''
     Categorical vs. Quantitative
     Cate -> nominal ( 이름 ) vs. ordinal ( 순서 ) 
     Quan -> interval ( 상대 ) vs. ratio ( 절대 ) 
     '''
-
-    def pclass_ordinal(self) -> object:
-        pass
-
-    def name_nominal(self) -> object:
-        pass
-
-    def sex_nominal(self) -> object:
-        pass
-
-    def age_ratio(self) -> object:
-        pass
-
-    def sib_sp_garbage(self) -> object:
-        self.drop_feature()
-
-    def parch_garbage(self) -> object:
-        self.drop_feature()
-
-    def ticket_garbage(self) -> object:
-        pass
-
-    def fare_ratio(self) -> object:
-        pass
-
-    def cabin_garbage(self) -> object:
-        pass
-
-    def embarked_nominal(self) -> object:
-        pass
+    @staticmethod
+    def pclass_ordinal(df) -> object:
+        return df
+    @staticmethod
+    def name_nominal(df) -> object:
+        return df
+    @staticmethod
+    def sex_nominal(df) -> object:
+        return df
+    @staticmethod
+    def age_ratio(df) -> object:
+        return df
+    @staticmethod
+    def sibsp_garbage(df) -> object:
+        TitanicModel.drop_feature()
+        return df
+    @staticmethod
+    def parch_garbage(df) -> object:
+        TitanicModel.drop_feature()
+        return df
+    @staticmethod
+    def ticket_garbage(df) -> object:
+        TitanicModel.drop_feature()
+        return df
+    @staticmethod
+    def fare_ratio(df) -> object:
+        return df
+    @staticmethod
+    def cabin_garbage(df) -> object:
+        TitanicModel.drop_feature()
+        return df
+    @staticmethod
+    def embarked_nominal(df) -> object:
+        return df
 
 
